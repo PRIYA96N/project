2aa253d17d3494bf0072d28dc1fa47fc58ee7adc
PRIYA96N <priyamalliga.11@gmail.com>
2016-08-24 15:07:09 +0530

    Update Stringrev.java

--- Stringrev.java
+++ Stringrev.java
@@ -1,9 +1,10 @@
+import java.util.*;
 public class Stringrev{
 
      public static void main(String []args){
       Scanner sc=new Scanner(System.in);
       String s=sc.nextLine();
-      String str=sc.StringBuffer(s).reverse().toString();
+      String str=new StringBuffer(s).reverse().toString();
       System.out.println(str);
      }
 }
