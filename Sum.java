17160f2689786a06912bac2caaa5a618003d4dca
PRIYA96N <priyamalliga.11@gmail.com>
2016-08-24 11:30:07 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ public class Sum {
+  public static void main(String[] args) { 
+    Scanner rc = new Scanner(System.in);
+    int a=rc.nextInt();
+    int b=0;
+    for(int i=1;i<=a;i++)
+    {   b+=i;
+        
+    }
+   System.out.println(b);
+   }
+   }
