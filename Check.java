e656140f4c7716a684f3cc4813d8227ed9d5b981
PRIYA96N <priyamalliga.11@gmail.com>
2016-08-24 10:16:49 +0530

    Create Check.java

---
+++ Check.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ public class Check {
+  public static void main(String[] args) { 
+    Scanner in = new Scanner(System.in);
+    System.out.print("the input is ");
+    int num = in.nextInt();
+    if(num%2== 0)
+    { 
+        System.out.println("Number is even");
+        }
+    else 
+    { 
+        System.out.println("Number is odd");
+    }
+   }
+   }
