55878612ad2e4bd300678ecc2623bd2f434cc0d6
PRIYA96N <priyamalliga.11@gmail.com>
2016-08-24 11:19:07 +0530

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ public class Print {
+  public static void main(String[] args) { 
+    //Scanner rc = new Scanner(System.in);
+    String a="Hello";
+    int b=1;
+    while(b<=5)
+    {
+        System.out.println(a);
+        b++;
+    }
+   
+   }
+   }
