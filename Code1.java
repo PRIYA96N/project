e98d964f644fb9459bc9596c4df1c5cf237e980a
PRIYA96N <priyamalliga.11@gmail.com>
2016-08-24 17:36:16 +0530

    Create Code1.java

---
+++ Code1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Code1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    
+	Scanner sc=new Scanner(System.in);
+	String s1="2";
+	String s2="3";
+	int a=Integer.parseInt(s1);
+	int b=Integer.parseInt(s2);
+	int d=a*b;
+	String c=Integer.toString(d);
+	System.out.println(c);
+	
+	}
+}
