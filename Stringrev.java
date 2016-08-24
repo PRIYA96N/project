6cc66dd01047e0e1d4fea8edd506f29647098f3b
PRIYA96N <priyamalliga.11@gmail.com>
2016-08-24 15:03:54 +0530

    Create Stringrev.java

---
+++ Stringrev.java
@@ -0,0 +1,9 @@
+public class Stringrev{
+
+     public static void main(String []args){
+      Scanner sc=new Scanner(System.in);
+      String s=sc.nextLine();
+      String str=sc.StringBuffer(s).reverse().toString();
+      System.out.println(str);
+     }
+}
